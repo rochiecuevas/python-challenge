adaa62267325c797e9631f018172e54895f5a468
Rochie Cuevas <rochiecuevas@gmail.com>
2018-09-22 23:00:36 -0700

    explore solutions using lists in csv module

--- PyBank/main.py
+++ PyBank/main.py
@@ -1,38 +1,60 @@
-# import modules
-import csv
-import pandas as pd
+# import module
+import csv 
 
 # open csv file
-csvfile = "03-Python_homework_PyBank_Resources_budget_data.csv"
-df = pd.read_csv(csvfile)
-df.columns = ["Date","Profit_Losses"]
-df.head() # first five rows
+with open("03-Python_homework_PyBank_Resources_budget_data.csv","r") as csvfile:
+    csvreader = csv.reader(csvfile, delimiter=',')
+    next(csvreader, None) # excludes the header row for hte csvreader
 
-x = df.Date.count() # number of entries
-y = df.Profit_Losses.sum() # net amount of Profits/Losses over the entire period
-z = df.Profit_Losses.diff() # date-by-date change
-ave = z.mean() # average of date-by-date differences (average change)
-z_min = int(z.min()) # greatest decrease in losses
-z_max = int(z.max()) # greatest increase in profits
-z_min_loc = df.loc[z ==z_min,"Date"].iloc[0]
-z_max_loc = df.loc[z ==z_max,"Date"].iloc[0]
+    # generate empty lists of Dates and Profit/Losses
+    Date = []
+    Profit_Loss = []
+    change = []
+
+    # populate the lists
+    for row in csvreader:
+        Date.append(row[0])
+        Profit_Loss.append(float(row[1]))
     
-# output
-print("Financial Analysis")
-print("------------------------------")
-print("Total Months: " + str(x))
-print("Total: $" + str(y))
-print("Average Change: $" + str(round(ave,2)))
-print("Greatest Increase in Profits: " + z_max_loc + " ($" + str(z_max) + ")")
-print("Greatest Decrease in Profits: " + z_min_loc + " ($" + str(z_min) + ")")
+    # number of months
+    no_months = len(Date) #86 months
+    
+    # net amount of profit/losses
+    net_amt = sum(Profit_Loss)
+    
+    # average change date-by-date for profit/losses
+    for i in range(1,no_months):
+        # create list of date-by-date changes in profit/losses
+        change.append(Profit_Loss[i] - Profit_Loss[i-1])
+    ave_change = sum(change) / (no_months - 1) # there are only 85 entries for differences
+    
+    # greatest increase in profit
+    gt_in = max(change)
+
+    # greatest decrease in profit
+    gt_dc = min(change)
+
+    # dates corresponding to the greatest changes
+    date_dc = Date[change.index(gt_dc) + 1] # to give the date for Profit_Loss[i-1]
+    date_in = Date[change.index(gt_in) + 1] # to give the date for Profit_Loss[i-1]
+
+# print out the financial analysis
+    print("Financial Analysis")
+    print("------------------------------")
+    print("Total Months: " + str(no_months))
+    print("Total: $" + str(int(net_amt)))
+    print("Average Change: $" + str(round(ave_change,2)))
+    print("Greatest Increase in Profits: " + date_in + " ($" + str(int(gt_in)) + ")")
+    print("Greatest Decrease in Profits: " + date_dc + " ($" + str(int(gt_dc)) + ")")
+
 
 # create new text file
-f = open("PyBank.txt","w")
-f.write("Financial Analysis")
-f.write("\n" + "------------------------------")
-f.write("\n" + "Total Months: " + str(x))
-f.write("\n" + "Total: $" + str(y))
-f.write("\n" + "Average Change: $" + str(round(ave,2)))
-f.write("\n" + "Greatest Increase in Profits: " + z_max_loc + " ($" + str(z_max) + ")")
-f.write("\n" + "Greatest Decrease in Profits: " + z_min_loc + " ($" + str(z_min) + ")")
-f.close()
+#f = open("PyBank.txt","w")
+#f.write("Financial Analysis")
+#f.write("\n" + "------------------------------")
+#f.write("\n" + "Total Months: " + str(x))
+#f.write("\n" + "Total: $" + str(y))
+#f.write("\n" + "Average Change: $" + str(round(ave,2)))
+#f.write("\n" + "Greatest Increase in Profits: " + z_max_loc + " ($" + str(z_max) + ")")
+#f.write("\n" + "Greatest Decrease in Profits: " + z_min_loc + " ($" + str(z_min) + ")")
+#f.close()
