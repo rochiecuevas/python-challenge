38c569b1cc1bafb820fc9e9c3c9fcaaa84772849
Rochie Cuevas <rochiecuevas@gmail.com>
2018-09-24 17:14:25 -0700

    first draft of python script for the PyPoll challenge

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -1,10 +1,52 @@
 # import modules
 import csv
 
-# open csv file
-with open("03-Python_homework_PyPoll_Resources_election_data.csv", newline = "") as csvfile:
-    csvreader = csv.reader(csvfile,delimiter = ",")
+print("Election Results")
+print("----------------------------")
 
-    # Read the header row first (skip this step if there is now header)
-    csv_header = next(csvreader)
-    print(f"CSV Header: {csv_header}")
+# open csv file
+with open("03-Python_homework_PyPoll_Resources_election_data.csv", "r") as csvfile:
+    csvreader = csv.DictReader(csvfile)
+    next(csvreader,None)
+
+    Poll_Results = [] # start with an empty set
+    for row in csvreader:
+        Poll_Results.append(row) # populate the list with dictionaries of voter-county-candidate combinations
+    
+    no_voters = len(Poll_Results) # number of voters
+
+    print("Total Votes: " + str(no_voters))
+    print("----------------------------")
+
+    Candidate = [] # empty list
+    for vote in Poll_Results: # populate the empty list of candidates
+        Candidate.append(vote["Candidate"])
+    
+    candidate_set = set(Candidate) # list of candidates
+
+    cand_vote = [] # empty list
+    for i in candidate_set:
+        cand_vote.append(Candidate.count(i)) # populate list of the number of votes per candidate
+
+    pct_vote = [] # empty list    
+    for j in cand_vote:    
+        pct_vote.append((float(j) / no_voters) * 100) # populate list of % votes per candidate
+    
+    win = max(pct_vote)
+
+    # create a zip file of the lists
+    election_results = [[x,y,z] for x,y,z in zip(candidate_set,pct_vote,cand_vote)]
+
+    # find the winning candidate
+    for row in election_results:
+        summary = row[0] + ": " + str(row[1]) +"%" + " (" + str(row[2]) + ")"
+        print(summary)
+        
+        if row[1] == win:
+            winner = row[0] 
+    
+    print("----------------------------")
+    print("Winner: " + winner)
+    print("----------------------------")
+
+        
